1985420924a96ee6332ea550b4a241594989d4be
shefat <shefatul.islam99@gmail.com>
2021-02-16 23:21:59 +0600

    task4

---
+++ MID_LAB_TASK_PHP_04/Task4.php
@@ -0,0 +1,14 @@
+<?php
+	$num1=5;
+	$num2=20;
+	$num3=15;
+	if($num1 > $num2){
+		echo "number 1 is larger";
+	}
+	elseif($num2 > $num3){
+		echo "number 2 is larger";
+	}
+	else{
+		echo "number 3 is larger";
+	}
+?>
