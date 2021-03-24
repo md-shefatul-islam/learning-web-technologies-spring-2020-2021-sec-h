013c0f735fab580c2c2076048a0b0dd1f4d6d440
shefat <shefatul.islam99@gmail.com>
2021-03-24 22:04:58 +0600

    final task 3

---
+++ FINAL_LAB_TASK_03_php/controller/logCheck.php
@@ -0,0 +1,42 @@
+<?php
+	session_start();
+	require_once('../model/userModel.php');
+
+	if(isset($_POST['submit'])){
+
+		$username = $_POST['username'];
+		$password = $_POST['password'];
+		if(isset($_POST['submit']))
+		{
+		$check = $_POST['check'];
+		}
+
+
+		if($username == "" || $password == ""){
+			echo "null submission...";
+		}else{
+
+			$status = validateUser($username, $password);
+			if($status){
+				$user = getUserByName($username, $password);
+
+				$_SESSION['flag'] = true;
+				$_SESSION['id'] = $user['id'];
+				$_SESSION['type'] = $user['type'];
+				$_SESSION['username'] = $username;
+				$_SESSION['password'] = $password;
+				$_SESSION['check'] = $check;
+
+				if($user['type'] == 'admin'){
+
+					header('location: ../view/adminhome.php');
+				}
+				else{
+					header('location: ../view/userhome.php');
+				}				
+			}else{
+				echo "invalid user";
+			}
+		}
+	}
+?>
